2726a38bec101039c170b12a94b26b93dbf2c06b
Sergio <Sergio@DESKTOP-PAIHCTG>
2024-04-02 19:35:41 +0200

    Commit 2

--- Entornos/src/equipos/Equipo.java
+++ Entornos/src/equipos/Equipo.java
@@ -1,5 +1,39 @@
 package equipos;
-
+import java.util.ArrayList;
 public class Equipo {
+	
+	    private String nombre;
+	    private ArrayList<Jugador> jugadores;
 
+	    public Equipo(String nombre) {
+	        this.nombre = nombre;
+	        this.jugadores = new ArrayList<>();
+	    }
+	    
+	    /**
+	     * Agrega un jugador al equipo.
+	     * @param jugador El jugador a agregar.
+	     */
+	    
+	    public void agregarJugador(Jugador jugador) {
+	        jugadores.add(jugador);
+	        System.out.println("Jugador agregado al equipo: " + jugador.getNombre());
+	    }
+	    
+	    
+	    /**
+	     * Elimina un jugador del equipo por su nombre.
+	     * @param nombreJugador El nombre del jugador a eliminar.
+	     */
+	    	    public void eliminarJugador(String nombreJugador) {
+	        for (int i = 0; i < jugadores.size(); i++) {
+	            Jugador jugador = jugadores.get(i);
+	            if (jugador.getNombre().equals(nombreJugador)) {
+	                jugadores.remove(i);
+	                System.out.println("Jugador eliminado del equipo: " + nombreJugador);
+	                return;
+	            }
+	        }
+	        System.out.println("No se encontró ningún jugador con ese nombre en el equipo.");
+	    }
 }
--- Entornos/src/equipos/Jugador.java
+++ Entornos/src/equipos/Jugador.java
@@ -1,5 +1,88 @@
 package equipos;
 
-public class Jugador {
+import java.util.Scanner;
 
+public class Jugador {
+	
+	private String nombre;
+    private int dorsal;
+    private String posicion;
+
+  
+
+    /**
+     * Constructor de la clase Jugador.
+     * @param nombre El nombre del jugador.
+     * @param dorsal El dorsal del jugador.
+     */
+    
+	public Jugador(String nombre, int dorsal) {
+        this.nombre = nombre;
+        this.dorsal = dorsal;
+    }
+    
+	  public String getNombre() {
+			return nombre;
+		}
+
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+
+		public int getDorsal() {
+			return dorsal;
+		}
+
+
+		public void setDorsal(int dorsal) {
+			this.dorsal = dorsal;
+		}
+
+
+		public String getPosicion() {
+			return posicion;
+		}
+
+
+		public void setPosicion(String posicion) {
+			this.posicion = posicion;
+		}
+		
+		 /**
+	     * Permite al usuario elegir la posición del jugador.
+	     */
+    public void elegirPosicion() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Elige la posición del jugador:");
+        System.out.println("1. Delantero");
+        System.out.println("2. Centrocampista");
+        System.out.println("3. Portero");
+        System.out.print("Ingrese el número correspondiente a la posición: ");
+        
+        int opcion = scanner.nextInt();
+        switch (opcion) {
+            case 1:
+                this.posicion = "Delantero";
+                break;
+            case 2:
+                this.posicion = "Centrocampista";
+                break;
+            case 3:
+                this.posicion = "Portero";
+                break;
+            default:
+                System.out.println("Opción no válida. Por favor, elige una opción válida.");
+                break;
+        }
+    }
+    /**
+     * Muestra informacion del jugador
+     */
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Dorsal: " + dorsal);
+        System.out.println("Posición: " + posicion);
+    }
 }
